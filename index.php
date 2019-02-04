c4bbc2e68ca2ef6fba1d3fa7bb45c7cfa8cdf260
Ricky Noel <jonryder1828@gmail.com>
2019-02-04 15:14:53 +0800

    added greetings

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="greetings.php" method="post">
+Name: <input type="text" name="name"><br>
+Favorite Color: <input type="text" name="color"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
